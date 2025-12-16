1e55742f5c76f443558fa19810abf77978b76604
gurhankuras <gurhankuras@hotmail.com>
2025-12-16 18:47:14 +0300

    get and update marketing version directly instead of broken increment_version_number

--- fastlane/helpers/apply_version_and_build.rb
+++ fastlane/helpers/apply_version_and_build.rb
@@ -1,7 +1,7 @@
 UI = FastlaneCore::UI unless defined?(UI)
 
 def apply_version_and_build!(xcodeproj:)
-  current_version = get_version_number(xcodeproj: xcodeproj)
+  current_version = get_marketing_version()
   UI.message("Current version: #{current_version}")
 
   new_version = resolve_version(current_version)
@@ -9,12 +9,7 @@ def apply_version_and_build!(xcodeproj:)
   if new_version != current_version
     UI.message("Updating version to: #{new_version}")
 
-    increment_version_number(
-      version_number: new_version,
-      xcodeproj: xcodeproj
-    )
-
-    # New version => reset build
+    set_marketing_version(version: new_version)
     increment_build_number(
       build_number: 1,
       xcodeproj: xcodeproj
@@ -22,11 +17,14 @@ def apply_version_and_build!(xcodeproj:)
   else
     UI.message("Keeping version: #{current_version}")
 
-    # Same version => increment build
     increment_build_number(
       xcodeproj: xcodeproj
     )
   end
 
+  final_version = get_marketing_version()
+  final_build = get_build_number(xcodeproj: xcodeproj)
+  
+  UI.important("🎯 Final version: #{final_version} (#{final_build})")
   new_version
 end
